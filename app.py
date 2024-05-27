e0e02d4934669eb4581b8154e05b10edeb236c6f
ELFAHIM96 <elfahimomar95@gmail.com>
2024-05-27 16:31:35 +0200

    change code

--- app.py
+++ app.py
@@ -1,2 +1,31 @@
 from langchain_community.llms import ollama
 
+from crewai import Agent, Task, Crew, Process
+
+model  = ollama(model="llama3")
+
+email = "nigerian prince sending some gold"
+
+# Create  first agent
+
+classifier = Agent(
+    role  = "email classifier",
+    goal  = "accurately classify emails based on thier imortance. give  every email one of this ratings: important, casual, or spam",
+    backstory="You are an AI assistant whose only job is to classify emails accurately and honestly. Do not be afraid to give emails bad rating if they are not important. your job is to help the user manage thier inbox",
+    verbose= True,
+    allow_delegation= False,
+    llm= model
+
+)
+
+# Create second agent*
+
+responder = Agent(
+    role  = "email responder",
+    goal  = "respond to emails in a way that is appropriate to the email's importance first, then casual emails, and ignore spam emails. and based on theimportance of mail write a concise and simple response, uf the email is rated 'important' write a formal responce , if the email is rated casual write a casual responce , and if the email is rated 'spam' ignore the email. no matter what, be very concise",
+    backstory="You are an AI assistant whose only job is to respond to emails accurately and honestlt . do not be afraid to ignore emails if they are not important. your job is to help the user manage thier inbox",
+    verbose= True,
+    allow_delegation= False,
+    llm= model
+    )
+
