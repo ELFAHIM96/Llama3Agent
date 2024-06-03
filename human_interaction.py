bf6d7b271708c52487e25bbb31969488589b374e
ELFAHIM96 <elfahimomar95@gmail.com>
2024-06-03 08:38:06 +0200

    changes

--- human_interaction.py
+++ human_interaction.py
@@ -10,10 +10,10 @@ def human_interaction(email: str, output: Dict[str, str]) -> Dict[str, str]:
     human_verification = input("Is the classification correct? (yes/no): ")
     
     if human_verification.lower() != 'yes':
-        classification = input("Enter the correct classification (important/casual/spam): ")
+        classification = input("Enter the correct classification based on mail  (important/casual/spam): ")
     
     print(f"Initial Response: {response}")
-    response_modification = input("Do you want to modify the response? (yes/no): ")
+    response_modification = input("Do you want to modify the response :? (yes/no): ")
     
     if response_modification.lower() == 'yes':
         response = input("Enter the modified response: ")
